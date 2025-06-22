728791493bb2c3e89c72e1fecaacb5ac57521a32
Alexander Kazanski <alexander1.15.1943@gmail.com>
2025-06-22 12:46:33 -0400

    feat: enhance meal details page with layout and styling

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,21 @@
+import Image from 'next/image';
+import classes from './page.module.css';
+
 export default function MealsDetails({params}) {
-  return <p>{params.mealSlug} - Meal Slug</p>
+  return <>
+    <header classes={classes.header}>
+      <div className={classes.image}>
+        <Image fill  />
+      </div>
+      <div className={classes.headerText}>
+        <h1>TITLE</h1>
+        <p className={classes.creator}>by <a href={`mailto:${}`}>NAME</a></p>
+        <p className={classes.summary}>SUMMARY</p>
+      </div>
+    </header>
+    <main>
+      <p className={classes.instructions}></p>
+    </main>
+    <p>{params.mealSlug} - Meal Slug</p>
+  </>
 }
