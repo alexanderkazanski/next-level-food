41230f6f408a660ea2f7effd249d40cf438f7708
Alexander Kazanski <alexander1.15.1943@gmail.com>
2025-06-22 11:46:28 -0400

    chore: update meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,6 +1,12 @@
 import Link from 'next/link';
 import classes from './page.module.css';
-export default function MealsPage() {
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+
+
+export default async function MealsPage() {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -15,7 +21,7 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        
+        <MealsGrid meals={meals} />
       </main>
     </>
   )
